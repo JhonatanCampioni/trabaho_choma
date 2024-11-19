00957dc54dfa25e7b9611c5069a39953366cb681
JhonatanCampioni <145516223+JhonatanCampioni@users.noreply.github.com>
2024-11-18 21:34:02 -0300

    Add files via upload

---
+++ trabalho_choma/src/main/java/com/example/trabalho_choma/TrabalhoChomaApplication.java
@@ -0,0 +1,13 @@
+package com.example.trabalho_choma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoChomaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoChomaApplication.class, args);
+	}
+
+}
